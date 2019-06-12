dc51c612063fec4a21e734c1d96e5b9719eeba48
Max-F <max.fullmer@outlook.com>
2019-06-12 14:23:35 -0700

    4 problem minumum

--- practice.js
+++ practice.js
@@ -14,8 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Michael',
+  age: 55
+};
 
+alert(me.age);
 ////////// PROBLEM 2 //////////
 
 /*
@@ -23,21 +27,31 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'jazz',
+  food: 'sushi',
+  person: 'Jesus',
+  book: 'The Screwtape Letters',
+  movie: 'Rio',
+  holiday: 'Christmas'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings["car"] = 'Stingray';
+favoriteThings["brand"] = 'Andriod';
+
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings["food"] = 'Chicken Nuggets';
+favoriteThings["book"] = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +63,17 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {};
+let item = 'firstPocket';
+
+backPack[item] = 'chapstick';
+backPack.color = 'grey';
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +81,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +102,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2["name"] = 'Bryan G. Smith';
+user2["email"] = 'bryan.smith@devmounta.in';
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
